698faea3bd7608b1dee6d84d84df8ebcca9482d5
dipto bkashi~ <diptibakshi72@gmail.com>
2024-11-16 01:45:23 +0600

    add footer

--- src/Componet/Pajes/HomePages/UserRevus/Reviwes.jsx
+++ src/Componet/Pajes/HomePages/UserRevus/Reviwes.jsx
@@ -1,28 +1,28 @@
 const Reviwes = () => {
   return (
-    <div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="flex gap-10 justify-center mb-11 ">
+      <div className="card bg-gradient-to-r from-cyan-500 to-blue-500 w-96 shadow-xl ">
         <div className="card-body">
           <h2 className="card-title">Good</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end"></div>
         </div>
       </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card bg-base-100 bg-gradient-to-r from-cyan-500 to-blue-500 w-96 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Good</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end"></div>
         </div>
       </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card bg-base-100 bg-gradient-to-r from-cyan-500 to-blue-500 w-96 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Good</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end"></div>
         </div>
       </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card bg-base-100 bg-gradient-to-r from-cyan-500 to-blue-500 w-96 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Good</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
